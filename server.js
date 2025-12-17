f354d1adee7507bd463b4db2984dbb78fadade19
Aziz62528 <aziz676744@gmail.com>
2025-12-17 11:34:37 +0200

    fixed html js files

--- server.js
+++ server.js
@@ -4,58 +4,28 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
 
 app.use(express.static("public"));
 
 let players = {};
-let it = null; // ebe
 
 io.on("connection", socket => {
-  console.log("Oyuncu bağlandı:", socket.id);
-
-  players[socket.id] = {
-    x: Math.random() * 500,
-    y: Math.random() * 500,
-    name: "Player",
-    color: "#ff0000",
-    skin: "square"
-  };
-
-  if (!it) it = socket.id;
+  players[socket.id] = { x:250, y:250, name:"Player" };
 
   socket.on("update", data => {
-    if (!players[socket.id]) return;
-
-    players[socket.id] = { ...players[socket.id], ...data };
-
-    // TAG (ebe yakalama)
-    if (it && socket.id !== it) {
-      const a = players[it];
-      const b = players[socket.id];
-      if (a && b && Math.abs(a.x - b.x) < 20 && Math.abs(a.y - b.y) < 20) {
-        it = socket.id;
-      }
-    }
-  });
-
-  socket.on("chat", data => {
-    io.emit("chat", data);
+    players[socket.id] = data;
   });
 
   socket.on("disconnect", () => {
-    console.log("Oyuncu çıktı:", socket.id);
     delete players[socket.id];
-    if (it === socket.id) {
-      it = Object.keys(players)[0] || null;
-    }
   });
 });
 
 setInterval(() => {
-  io.emit("state", { players, it });
-}, 30);
+  io.emit("players", players);
+}, 50);
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log("Server çalışıyor");
-});
+server.listen(process.env.PORT || 3000);
